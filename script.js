3498e4691e9a2694042b2b076658fbde4b88d399
Ektamadaan <ektamadaan45@gmail.com>
2023-12-15 22:12:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-const makeChange = (c) => {
+const makeChange = (amountInCents) => {
   // your name here
+	const quarterValue = 25;
+    const dimeValue = 10;
+    const nickelValue = 5;
+    const pennyValue = 1;
+
+    // Initialize the variables to store the number of each coin
+    let q = 0; // quarters
+    let d = 0; // dimes
+    let n = 0; // nickels
+    let p = 0; // pennies
+
+    // Calculate the number of each coin
+    while (amountInCents > 0) {
+        if (amountInCents >= quarterValue) {
+            q++;
+            amountInCents -= quarterValue;
+        } else if (amountInCents >= dimeValue) {
+            d++;
+            amountInCents -= dimeValue;
+        } else if (amountInCents >= nickelValue) {
+            n++;
+            amountInCents -= nickelValue;
+        } else {
+            p++;
+            amountInCents -= pennyValue;
+        }
+    }
+
+    // Create and return the result object
+    const result = { q, d, n, p };
+    return result;
+
 };
 
 // Do not the change the code below
